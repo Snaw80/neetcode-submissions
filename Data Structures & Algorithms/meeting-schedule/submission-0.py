6a9acc226c96943d08f2b9e0db99dd4870e75bc6
Lefebvre Arthur <34581821+Snaw80@users.noreply.github.com>
2026-06-15 09:19:11 +0200

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+        pre = 0
+        for i in intervals:
+            if pre > i.start:
+                return False
+            pre = i.end
+        return True
